e5217d48b3c448c116d5957e6c91dac1e3e0477e
Mojtaba Hafezi <mojtaba.hafezi@hotmail.de>
2018-12-10 22:56:37 +0100

    Added Point class
    Added Point class with
    - save and load functionality for template containers
    - different methods for calculating zero distance and point distances
    - overloaded operators

---
+++ UtilityClasses/Point.cpp
@@ -0,0 +1,83 @@
+#include "pch.h"
+#include "Point.h"
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+void Point::set(const double x, const double y)
+{
+	m_x = x;
+	m_y = y;
+}
+
+void Point::print() const
+{
+	std::cout << "(" << m_x << ", " << m_y << ")" << std::endl;
+}
+
+double Point::getZeroDistance() const
+{
+	return sqrt(m_x*m_x + m_y * m_y);
+}
+
+double Point::getDistanceToPoint(const Point &p) const
+{
+	return sqrt(pow((m_x - p.m_x), 2) + pow((m_y - p.m_y), 2));
+}
+
+const Point Point::getMiddlePoint(const Point &p) const
+{
+	double x, y;
+	x = (m_x + p.m_x) / 2;
+	y = (m_y + p.m_y) / 2;
+	return Point(x, y);
+}
+
+std::string Point::toString() const
+{
+	std::stringstream temp;
+	temp << "(" << m_x << ", " << m_y << ")";
+	return temp.str();
+}
+
+std::ostream& operator<<(std::ostream &out, const Point &p)
+{
+	out << p.toString();
+	return out;
+}
+
+//Making useable for save and load
+std::istream& operator>>(std::istream &in,  Point &p)
+{
+	std::string temp;
+	std::getline(in, temp);
+	//find delimiter
+	size_t pos = temp.find(",");
+	std::string beg, end;
+	//create substrings
+	if (pos != std::string::npos)
+	{
+		beg = temp.substr(0, pos);
+		end = temp.substr(pos + 1, temp.length());
+		//remove ",", " ", "(" from substring beg
+		beg.erase(std::remove(beg.begin(), beg.end(), ','), beg.end());
+		beg.erase(std::remove_if(beg.begin(), beg.end(), isspace), beg.end());
+		beg.erase(std::remove(beg.begin(), beg.end(), '('));
+
+		//remove ")", " ", ","
+		end.erase(std::remove(end.begin(), end.end(), isspace));
+		end.erase(std::remove(end.begin(), end.end(), ')'));
+		end.erase(std::remove(end.begin(), end.end(), ','));
+
+		float x = float(std::stof(beg));
+		float y = float(std::stof(end));
+		p.set(x, y);
+		return in;
+	}
+	else
+	{
+		//error while loading due to wrong type/delimiter
+		p.set(0, 0);
+		return in;
+	}
+}
---
+++ UtilityClasses/Point.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+class Point
+{
+private:
+	double m_x, m_y;
+public:
+	//Constructors
+	Point(const double x, const double y) { set(x, y);}
+	Point() { set(0, 0);} //don't use for initialisation of many points
+
+	//Methods
+	void set(const double a, const double b);
+	void print() const;
+	std::string toString() const;
+	double getX() const { return m_x; }
+	double getY() const { return m_y; }
+	double getDistanceToPoint(const Point &p) const;
+	const Point getMiddlePoint(const Point &p) const;
+	double getZeroDistance() const;
+
+	//Operators
+	Point operator+(const Point &a) const { return Point(m_x + a.m_x, m_y + a.m_y); }
+	Point operator-(const Point &a) const { return Point(m_x - a.m_x, m_y - a.m_y); }
+	Point& operator+=(const Point &rhs) { *this = *this + rhs; return *this; }
+	Point& operator-=(const Point &rhs) { *this = *this - rhs; return *this; }
+
+	friend std::ostream& operator<<(std::ostream &out, const Point &p);
+	friend std::istream& operator>>(std::istream &in, const Point &p);
+
+
+
+
+};
+
